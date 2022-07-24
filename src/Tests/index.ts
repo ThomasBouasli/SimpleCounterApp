6c86dd7067afe533051e5fd0d371abc76146c870
Thomas Jardane de Oliveira Bouasli <78673858+ThomasBouasli@users.noreply.github.com>
2022-07-24 16:24:15 -0300

    Initial commit

---
+++ src/Tests/index.ts
@@ -0,0 +1,10 @@
+import '@Application'
+import '@Entity'
+import '@Error'
+import '@Feature'
+import '@Repository'
+import '@Util'
+
+export function sum(a: number, b: number) {
+    return a + b
+}
